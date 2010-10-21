b31fd17bc8f499e6d89a3e438aff0f25256c4103
Mike Tierney <mike@panpainter.com>
2010-10-21 12:49:05 -0700

    More precise time calculations.

--- app.rb
+++ app.rb
@@ -14,10 +14,17 @@ end
 
 helpers do
   def days_left
-    now = Time.now
-    today = Date.new(now.year, now.month, now.day)
-    goal = Date.new(2010, 10, 28)
+    # now = Time.now
+    # today = Date.new(now.year, now.month, now.day)
+    # goal = Date.new(2010, 10, 28)
+    # offset = goal - today
+    # offset.to_i
+    today = DateTime.now
+    goal = DateTime.new(2010,10,28,12,30,00)
     offset = goal - today
-    offset.to_i
+    hours,minutes,seconds,frac = Date.day_fraction_to_time(offset)
+    days = hours / 24
+    hours = hours % 24
+    "#{days} days, #{hours} hours, #{minutes} minutes and #{seconds} seconds"
   end
 end
